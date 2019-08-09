0d23f7c9f8330f0f25a360fa19ed62a41e2f4ddf
Ciphergyst <Jeremy McGrath>
2019-08-09 14:22:19 -0700

    Submitting & continuing on stretch problems

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function myName (firstName, lastName) {
+  var nameIntro = "My name is ";
+  function makeFullName () {
+    return nameIntro + firstName + " " + lastName;
+  }
+return makeFullName ();
+}
+console.log(myName("Jeremy", "McGrath"));
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
