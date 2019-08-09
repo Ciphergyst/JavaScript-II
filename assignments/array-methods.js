63cf22c8f0c8fbb24746c5830f0ab61076238a34
Ciphergyst <Jeremy McGrath>
2019-08-09 11:18:19 -0700

    Finished array-methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -72,15 +72,16 @@ console.log(allCaps);
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = runners.filter((data) => {
-  return data.shirt_size ==="L";
+  return data.shirt_size === "L";
 });
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = runners.reduce((donTally, item) => {
-  return donTally += item.donation;
-});
+  return donTally + item.donation
+}, 0);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -93,5 +94,14 @@ const lostItem = runners.find((item) => {
 });
 console.log(lostItem)
 // Problem 2
-
+// The event director wants to send a special gift to everyone that donated more than 50, can you filter out a list of those runners information?
+const filteredDonations = runners.filter((item) => {
+  return item.donation > 50
+})
+console.log(filteredDonations)
 // Problem 3
+// The Marketing department wants to list all of the companies that sponsored runners on our website, can you output just a list of company names?
+const companyNames = runners.map((item) => {
+  return item.company_name
+})
+console.log(companyNames)
