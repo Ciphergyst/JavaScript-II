7291f1406fc634725d5564af0279870321101759
Ciphergyst <Jeremy McGrath>
2019-08-12 14:10:44 -0700

    final submit

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,16 +13,28 @@ console.log(myName("Jeremy", "McGrath"));
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+const counter = (closure = 0) => {
   // Return a function that when invoked increments and returns a counter variable.
+  let val = closure;
+  return function counter(){
+    val += 1;
+    console.log(val);
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+ const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let counter = 0;
+  console.log(counter);
+  let increment = () => counter += 1;
+  let decrement = () => counter -= 1;
+  let resObj = {increment, decrement};
+
+  return resObj;
 };
